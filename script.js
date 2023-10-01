b95f39da2455bc2f932a2e248642ddeac3809b45
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2023-10-01 14:51:41 +0530

    added a new hamburger menu

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function toggleMenu(){
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
     menu.classList.toggle("open");
-    icon.classList.toggle("open")
+    icon.classList.toggle("open");
+}
 
-
-
+function toggleMenu2(){
+    const checkBox = document.querySelector('.toggler');
+    checkBox.click();
 }
